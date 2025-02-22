90d076106a6b9e9a22c5e2e28cbc51edf608c7c2
JRSALVES <jeruso.nascimento@outlook.com>
2025-02-22 11:06:51 -0300

    Correçãocodigo capitulo 3 lixeira deixando com opções de abri r/fechar tampa

--- html/web3/lixeira.js
+++ html/web3/lixeira.js
@@ -2,9 +2,9 @@
 document.querySelector("svg").addEventListener('click', () => {
     
     // Ao clicar no <svg>, adiciona a classe "animated" ao elemento com id "tampa"
-    document.querySelector("#tampa").classList.add("animated");
+    document.querySelector("#tampa").classList.toggle("animated");
     
     // Também adiciona a classe "animated" ao elemento com id "lata"
-    document.querySelector("#lata").classList.add("animated");
+    document.querySelector("#lata").classList.toggle("animated");
 
 });
